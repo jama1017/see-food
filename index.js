a5182873669b48ad3822934877db6e8e0b839369
Jiaju Ma <jiaju_ma@brown.edu>
2023-09-02 21:17:33 -0700

    custom css and updated content

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // a function that gets called when the page is loaded
 window.onload = function() {
     var cfInstance = cf.ConversationalForm.startTheConversation({
+        // theme: "dark",
         formEl: document.getElementById("myform"),
         context: document.getElementById("cf-context"),
+        preventAutoFocus: true,
+        // loadExternalStyleSheet: false,
     });
 }
