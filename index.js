8ffd716523cf4c71275cc33e364b7851dff2f54b
Jiaju Ma <jiaju_ma@brown.edu>
2023-09-09 23:36:30 -0400

    robot and user image update. style update. bug fix

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // a function that gets called when the page is loaded
-window.onload = function() {
+window.onload = function () {
     var cfInstance = cf.ConversationalForm.startTheConversation({
-        // theme: "dark",
         formEl: document.getElementById("myform"),
         context: document.getElementById("cf-context"),
-        preventAutoFocus: true,
+        userImage: "./img/user_image.png",
+        robotImage: "./img/robot_image.png",
+        // theme: "dark",
         // loadExternalStyleSheet: false,
+        preventAutoFocus: true,
     });
 }
+
+
